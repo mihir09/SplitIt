49ad51daf3a9eb601e31dd9aae7fb3b9b62579b8
mihir09 <mj7774542@gmail.com>
2023-12-28 01:20:41 -0800

    Updated loading spinner and added expense-details

--- splitIt-app/src/app/group/group.module.ts
+++ splitIt-app/src/app/group/group.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GroupRoutingModule } from './group-routing.module';
 import { DatePipe } from '@angular/common';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
-  
   ],
   imports: [
     CommonModule,
     GroupRoutingModule,
+    MatDialogModule,
   ],
   providers: [DatePipe],
 })
