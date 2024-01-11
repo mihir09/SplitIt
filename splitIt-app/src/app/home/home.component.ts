2ab8d32637c60e2620a4f895d98b38a00dd0a9fd
mihir09 <mj7774542@gmail.com>
2024-01-10 23:44:11 -0800

    Added google analytics

--- splitIt-app/src/app/home/home.component.ts
+++ splitIt-app/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { UsersService } from '../users.service';
 import { AuthService } from '../auth.service';
+import { DOCUMENT } from '@angular/common';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationEnd, Router, RouterState } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +14,15 @@ export class HomeComponent {
   groups: any[] = [];
   showCreateGroup: boolean = false;
 
-  constructor(private usersService: UsersService, private authService: AuthService) {
+  constructor(
+    private usersService: UsersService,
+    private authService: AuthService,
+    private router: Router,
+    private titleService: Title,
+    @Inject(DOCUMENT) private document: Document
+    ) {
     this.loadUserGroups();
+    this.handleRouteEvents();
   }
 
   ngOnInit(): void {
@@ -32,4 +42,30 @@ export class HomeComponent {
   toggleCreateGroup() {
     this.showCreateGroup = !this.showCreateGroup;
   }
+
+  handleRouteEvents() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const title = this.getTitle(this.router.routerState, this.router.routerState.root).join('-');
+        this.titleService.setTitle(title);
+        gtag('event', 'page_view', {
+          page_title: title,
+          page_path: event.urlAfterRedirects,
+          page_location: this.document.location.href
+        })
+      }
+    });
+  }
+
+  getTitle(state: RouterState, parent: ActivatedRoute): string[] {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data['title']) {
+      data.push(parent.snapshot.data['title']);
+    }
+    if (state && parent && parent.firstChild) {
+      data.push(...this.getTitle(state, parent.firstChild));
+    }
+    return data;
+  }
+
 }
