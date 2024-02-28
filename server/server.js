b69c2c15d1b7724c88b0bfa24532ef45761a05ea
mihir09 <mj7774542@gmail.com>
2024-02-28 14:23:05 -0800

    Updating cors

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ mongoose.connect(uri, {
 });
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://splititapp.netlify.app');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
