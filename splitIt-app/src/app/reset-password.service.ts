b8e0919d7a292c3b7b4095357cd90aa3bee6f4d1
mihir09 <mj7774542@gmail.com>
2024-03-22 09:35:01 -0700

    Updated Reset password service

--- splitIt-app/src/app/reset-password.service.ts
+++ splitIt-app/src/app/reset-password.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ResetPasswordService {
-  private apiUrl = 'http://localhost:3000';
+  private apiUrl = 'https://gray-angry-dalmatian.cyclic.app';
 
   constructor(private http: HttpClient) { }
 
