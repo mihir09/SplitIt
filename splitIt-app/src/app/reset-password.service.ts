725d8128c29c70762fb8bcc93fe2b41c41cc2c0e
Mihir <mj7774542@gmail.com>
2025-12-14 01:21:00 -0500

    Changed Server

--- splitIt-app/src/app/reset-password.service.ts
+++ splitIt-app/src/app/reset-password.service.ts
@@ -6,8 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ResetPasswordService {
-  private apiUrl = 'http://localhost:3000/api';
-  // private apiUrl = 'https://split-it-server.onrender.com/api';
+  private apiUrl = 'https://split-it-server.onrender.com/api';
 
   constructor(private http: HttpClient) { }
 
